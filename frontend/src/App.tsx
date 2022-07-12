3ca7da3cd831ff8584ad50da2b9cf398af8dcf3e
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-12 20:54:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import NotificationButton from './components/NotificationButton'
 function App() {
   return (
     <>
+    <h1>Hello world !!!</h1>
+      <NotificationButton  />
       <NotificationButton  />
     </>
   )
