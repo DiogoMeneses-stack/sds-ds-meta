446893156101faee6eadedb62116a05c4682f8e6
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-12 20:52:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import NotificationButton from "./components/NotificationButton";
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   return (
     <>
-      <h1>Hello World !!!</h1>
       <NotificationButton  />
     </>
   )
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,13 +1,13 @@
-import icon from "./assets/img/notification.svg";
+import icon from '../../assets/img/notification-icon.svg'
 
 function NotificationButton() {
   return (
-    <>
+      <>
       <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
-      </div>
+      <img src={icon} alt="Notificar" />
+    </div>
     </>
-  );
+  )
 }
 
 export default NotificationButton;
