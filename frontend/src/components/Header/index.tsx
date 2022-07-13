7015e424f49020f70082f437ac4a2e3f38e87db1
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-12 21:43:05 -0300

    Other components.

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="dsmeta-logo-container">
+          <img src={logo} alt="DSMeta" />
+          <h1>DSMeta</h1>
+          <p>
+            Desenvolvido por
+            <a href="https://www.instagram.com/diogo_meneses_">
+               @diogo_meneses_
+            </a>
+          </p>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header;
