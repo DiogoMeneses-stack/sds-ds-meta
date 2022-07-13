6189831f38f8cadc8054a4a5456aac00362cd53f
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-13 19:52:51 -0300

    Twilio SMS

--- backend/src/main/java/com/diogomenesesstack/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/diogomenesesstack/dsmeta/controllers/SaleController.java
@@ -4,25 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diogomenesesstack.dsmeta.entities.Sale;
 import com.diogomenesesstack.dsmeta.services.SaleService;
+import com.diogomenesesstack.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
+
+	@Autowired
+	private SaleService service;
 	
 	@Autowired
-	private SaleService service; 
-	
+	private SmsService smSservice;
+
 	@GetMapping
-	public Page <Sale> findSales(
-			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
-			@RequestParam(value = "maxDate", defaultValue = "")String maxDate, 
-			Pageable pageble){
+	public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageble) {
 		return service.findSales(minDate, maxDate, pageble);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smSservice.sendSms(id);
 	}
 }
