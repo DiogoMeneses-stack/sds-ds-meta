93895e710628edbb078784d969f92f7791f38975
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-13 18:01:51 -0300

    API Test.

---
+++ backend/src/main/java/com/diogomenesesstack/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.diogomenesesstack.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diogomenesesstack.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
